5bd6657939667f0249a6825b849ef0fe6aa86e91
hL <1263164021@qq.com>
2023-11-01 16:52:05 +0800

    增加调整任务队列大小和线程池大小函数

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -1,17 +1,56 @@
 #include"ThreadPool.h"
 #include<iostream>
-void ThreadPool::work(){
+
+//工作线程的函数
+void ThreadPool::work(std::size_t workerNumber){
     while(true){
         std::function<void()>task;
+        bool notify;
         {
+            //获取队列锁并等待条件变量满足
             std::unique_lock<std::mutex>lock(tasksQueueMutex);
-            conditionVariable.wait(lock,
-                [this]{return this->threadPoolStop||!this->tasksQueue.empty();}
+            conditionConsumers.wait(lock,
+                [this,workerNumber]
+                {
+                    return this->threadPoolStop||
+                    !this->tasksQueue.empty()||
+                    poolSize<workerNumber+1;
+                }
             );
-            if(this->threadPoolStop&&this->tasksQueue.empty())
-                return;
-            task=std::move(tasksQueue.front());
-            tasksQueue.pop();
+            //处理线程池缩减或关闭
+            if((this->threadPoolStop&&this->tasksQueue.empty())||
+                (!this->threadPoolStop&&poolSize<workerNumber+1)){
+                    //分离这个线程
+                    this->workers[workerNumber].detach();
+
+                    //减少工作线程的数量
+                    while(this->workers.size()>poolSize&&!this->workers.back().joinable()){
+                        this->workers.pop_back();
+                    }
+
+                    //如果工作线程为空,唤醒析构函数中等待的条件
+                    if(this->workers.empty()){
+                        this->conditionConsumers.notify_all();
+                    }
+                    return ;
+                    
+                    
+            }else if(!this->tasksQueue.empty()){
+                task=std::move(this->tasksQueue.front());
+                this->tasksQueue.pop();
+                notify=this->tasksQueue.size()+1==maxQueueSize||this->tasksQueue.empty();
+            }else
+                continue;
+                
+
+        }
+        //减少正在执行的任务数量
+        HandleInFlightDecrement guard(*this);
+
+        //如果符合通知生产任务的条件
+        if(notify){
+            std::unique_lock<std::mutex> lock(this->tasksQueueMutex);
+            conditionProducers.notify_all();
         }
 
         //**捕获并处理异常**
@@ -25,25 +64,102 @@ void ThreadPool::work(){
     }
 }
 
+//启动工作线程
+void ThreadPool::startWorker(std::size_t workerNumber, std::unique_lock<std::mutex>const &lock){
+    //确保当前线程持有队列锁并且锁的指针指向当前线程池的队列锁
+    assert(lock.owns_lock()&&lock.mutex()==&this->tasksQueueMutex);
+    //确保工作线程数量不超过工作线程队列的大小
+    assert(workerNumber<=this->workers.size());
 
-ThreadPool::ThreadPool(size_t threadsNumber){
+    if(workerNumber<this->workers.size()){
+        std::thread & worker=this->workers[workerNumber];
+        //如果线程可加入，则启动线程
+        if(!worker.joinable()){
+            
+            worker=std::thread([this,workerNumber]{work(workerNumber);});
+        }
+    }else{
+        // 将新的工作线程加入到工作线程向量中
+        this->workers.emplace_back([this,workerNumber]{work(workerNumber);});
+    }
+
+}
+
+ThreadPool::ThreadPool(size_t threadsNumber):poolSize(threadsNumber),inFlight(0){
     //把工作函数加入工作线程数组中
+    std::unique_lock<std::mutex>lock(this->tasksQueueMutex);
     for(size_t i=0;i<threadsNumber;i++){
-        this->workers.emplace_back([this]{work();});
+        startWorker(i,lock);
+    }
+
+}
+void ThreadPool::setQueueSizeLimit(std::size_t limit){
+    std::unique_lock<std::mutex>lock(this->tasksQueueMutex);
+    
+    if(threadPoolStop){
+        return;
+    }
+    std::size_t const oldLimit=maxQueueSize;
+    maxQueueSize=std::max(limit,std::size_t(1));
+    if(oldLimit<maxQueueSize){
+        conditionProducers.notify_all();
+    }
+}
+void ThreadPool::setPoolSize(std::size_t limit){
+    if(limit<1){
+        limit=1;
+    }
+
+    std::unique_lock<std::mutex>lock(this->tasksQueueMutex);
+    if(threadPoolStop)
+        return;
+    
+    std::size_t const oldSize=poolSize;
+    assert(this->workers.size()>=oldSize);
+
+    poolSize=limit;
+    if(poolSize>oldSize){
+        //创建新的工作线程
+        //有可能在池大小减小后仍在运行，这样的线程将继续运行
+        for(std::size_t i=oldSize;i!=poolSize;i++){
+            startWorker(i,lock);
+        }
+    }else if(poolSize<oldSize){
+        // 通知所有工作线程开始缩小
+        this->conditionConsumers.notify_all();
     }
 }
 
+//等待工作队列为空
+void ThreadPool::waitUntilTasksQueueIsEmpty(){
+    std::unique_lock<std::mutex> lock(this->tasksQueueMutex);
+    this->conditionProducers.wait(lock,
+        [this]{return this->tasksQueue.empty();}    
+    );
+}
 
-
+//等待所有任务完成
+void ThreadPool::waitUntilNothingInFlight(){
+    std::unique_lock<std::mutex> lock(this->inFlightMutex);
+    this->inFlightCondition.wait(lock,
+        [this]{return this->inFlight==0;}
+    );
+}
 
 ThreadPool::~ThreadPool(){
 
-    {
-        std::unique_lock<std::mutex>lock(tasksQueueMutex);
-        threadPoolStop=true;
-    }
-    conditionVariable.notify_all();
-    for(auto&worker:workers){
-        worker.join();
-    }
+    //获取任务队列的锁
+    std::unique_lock<std::mutex>lock(tasksQueueMutex);
+    //设置线程池停止
+    threadPoolStop=true;
+    //线程池大小设置为0
+    poolSize=0;
+    //唤醒所有消费者线程
+    conditionConsumers.notify_all();
+    //唤醒所有生产者线程
+    conditionProducers.notify_all();
+    //等待队列为空
+    conditionConsumers.wait(lock,[this]{return this->workers.empty();});
+    //检查所有任务都释放
+    assert(inFlight==0);
 }
--- ThreadPool.h
+++ ThreadPool.h
@@ -5,8 +5,11 @@
 #include <memory>
 #include <thread>
 #include <mutex>
+#include<atomic>
+#include<cassert>
 #include <condition_variable>
 #include <future>
+#include <algorithm>
 #include <functional>
 #include <stdexcept>
 
@@ -17,11 +20,22 @@ public:
 
     //将函数添加到工作线程中
     template<class F,class... Args>
-    // auto addTasksQueue(F&& f, Args&&... args)
-    //     -> std::future<typename std::result_of<F(Args...)>::type>;
     std::future<typename std::result_of<F(Args...)>::type> addTasksQueue(F&& f, Args&&... args) ;
-    //析构函数，释放所有线程
+    
+    //等待直到任务队列为空
+    void waitUntilTasksQueueIsEmpty();
+
+    //等待直到没有任务在执行
+    void waitUntilNothingInFlight();
+
+    //析构函数，关闭线程池，释放所有线程
     ~ThreadPool();
+
+    //设置线程池的大小
+    void setPoolSize(std::size_t limit);
+
+    //设置任务队列的大小限制
+    void setQueueSizeLimit(std::size_t limit);
 
 private:
     //工作线程数组
@@ -30,16 +44,53 @@ private:
     //任务数组
     std::queue<std::function<void()>>tasksQueue;
 
-    //互斥锁
-    std::mutex tasksQueueMutex;
+    //用于同步的互斥锁
+    std::mutex tasksQueueMutex;//任务队列互斥锁
+    std::condition_variable conditionProducers;//生产者条件变量
+    std::condition_variable conditionConsumers;//消费者条件变量
+
+    //线程池的限制大小
+    std::size_t poolSize;
+
+    //任务队列的最大长度限制
+    std::size_t maxQueueSize=100000;
 
     //条件变量
     std::condition_variable conditionVariable;
 
     //工作函数
-    void work();
+    void work(std::size_t workerNumber);
+    void startWorker(std::size_t workerNumber,std::unique_lock<std::mutex>const &lock);
 
+    //停止信号,用于关闭线程池
     bool threadPoolStop=false;
+
+    //用于跟踪任务执行状态的互斥锁和条件变量
+    std::mutex inFlightMutex;
+    std::condition_variable inFlightCondition;
+    std::atomic<std::size_t> inFlight; //原子变量
+
+    //用于处理任务执行状态的自动减少的结构体
+    struct HandleInFlightDecrement{
+        ThreadPool &threadPool;
+        // 初始化线程池
+        HandleInFlightDecrement(ThreadPool& threadPool):threadPool(threadPool){}
+        ~HandleInFlightDecrement(){
+            //减少任务执行状态计数，使用原子操作
+            std::size_t prev=std::atomic_fetch_sub_explicit(
+                &threadPool.inFlight,
+                std::size_t(1),
+                std::memory_order_acq_rel
+            );
+            //如果减少前计数为1，表示没有执行的任务
+            if(prev==1){
+                std::unique_lock<std::mutex>guard(threadPool.inFlightMutex);
+                //通知等待的线程，所有任务都已经执行完毕
+                threadPool.inFlightCondition.notify_all();
+            }
+        }
+    };
+
     
 };
 
@@ -58,6 +109,17 @@ std::future<typename std::result_of<F(Args...)>::type> ThreadPool::addTasksQueue
     auto res=task->get_future();
     {
         std::unique_lock<std::mutex> lock(tasksQueueMutex);
+
+        //检查是否当前等待队列的大小是否超过了队列最大限制
+        if(tasks.size()>=this->maxQueueSize){
+            //等待一些任务被完成或线程池停止
+            conditionProducers.wait(lock,
+                [this]{
+                    return tasksQueue.size()<maxQueueSize||threadPoolStop;
+                }
+            );
+        }
+
         // conditionVariable.wait(lock);
         if(threadPoolStop){
             throw std::runtime_error("线程池已停止！");
@@ -65,7 +127,11 @@ std::future<typename std::result_of<F(Args...)>::type> ThreadPool::addTasksQueue
         tasksQueue.emplace([task](){(*task)();});
     }
 
-    conditionVariable.notify_one();
+    //将任务执行状态计数加1
+    std::atomic_fetch_add_explicit(&inFlight,std::size_t(1),std::memory_order_relaxed);
+    
+    //通知一个线程来处理任务队列中的任务
+    conditionConsumers.notify_one();
 
     return res;
 }
